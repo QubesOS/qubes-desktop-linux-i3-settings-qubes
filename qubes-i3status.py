7c2795da3700efac6b486c037252fa15ebe04590
Ben Grande <ben@invisiblethingslab.com>
2026-07-29 10:41:06 +0200

    Fix pylint and black error and improve netvm query

--- qubes-i3status.py
+++ qubes-i3status.py
@@ -20,50 +20,66 @@ def json_output(name, full_text, color=None):
 
 
 def status_net():
-    netvms = [
-        vm for vm in app.domains if vm.features.get("servicevm") == "1" and not vm.netvm
+    """
+    Query network information such as IP address and the network connected to.
+    """
+    running_netvms = [
+        qube
+        for qube in app.domains
+        if not getattr(qube, "netvm", None)
+        and getattr(qube, "provides_network", False)
+        and qube.is_running()
     ]
     net_status = []
 
-    for netvm in netvms:
-        if not netvm.is_running():
-            continue
-        stdout, _ = netvm.run("ip -o -f inet addr")
-        for line in stdout.decode("utf-8").splitlines():
+    for qube in running_netvms:
+        # TODO: problematic run without timeout.
+        stdout, _ = qube.run("ip -o -f inet addr")
+        for line in stdout.decode("ascii").splitlines():
             match = re.match(
-                r"\d+: ((ens|eth|wl)\S+)(\s+)inet (\d+\.\d+\.\d+\.\d+)\/\d{1,2}", line
+                r"\d+: ((ens|eth|wl)\S+)(\s+)inet (\d+\.\d+\.\d+\.\d+)\/\d{1,2}",
+                line,
             )
-            if match:
-                device_name = match.group(1)
-                ip = match.group(4)
-                net_info = {
-                    "device_name": device_name,
-                    "info": f"{netvm.name}: {ip}",
-                }
-                if device_name.startswith("wl"):
-                    iw_info, _ = netvm.run("iw dev {} link".format(device_name))
-                    ssid = None
-                    for iw_line in iw_info.decode("utf-8").splitlines():
-                        # we allow only word and space characters
-                        ssid_match = re.match(r"^\s+SSID: ([\w\s]*)$", iw_line)
-                        if ssid_match:
-                            ssid = iw_line.split("SSID:")[1].strip()
-                            break
-                    if ssid:
-                        net_info["info"] += f" {ssid}"
+            if not match:
+                continue
+            device_name = match.group(1)
+            ip = match.group(4)
+            net_info = {
+                "device_name": device_name,
+                "info": f"{qube.name}: {ip}",
+            }
+            if device_name.startswith("wl"):
+                # TODO: problematic run without timeout.
+                iw_info, _ = qube.run(f"iw dev {device_name} link")
+                ssid = None
+                for iw_line in iw_info.decode("ascii").splitlines():
+                    # we allow only word and space characters
+                    ssid_match = re.match(r"^\s+SSID: ([\w\s]*)$", iw_line)
+                    if ssid_match:
+                        ssid = iw_line.split("SSID:")[1].strip()
+                        break
+                if ssid:
+                    net_info["info"] += f" {ssid}"
 
-                net_status.append(
-                    json_output(net_info["device_name"], net_info["info"])
-                )
+            net_status.append(
+                json_output(net_info["device_name"], net_info["info"])
+            )
     return net_status
 
 
 def status_time():
+    """
+    Get current time.
+    """
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return json_output("time", current_time)
 
 
 def status_bat():
+    """
+    Get battery percentage and if charging or not. Color the percentage
+    depending on health.
+    """
     batteries = list(Path("/sys/class/power_supply").glob("BAT*"))
     if not batteries:
         return
@@ -87,7 +103,9 @@ def status_bat():
                     charge_full = battery / "charge_full"
                     if charge_full.exists():
                         charge_full = int(charge_full.read_text().strip())
-                        accum_full_mWh += (voltage_now / 1000) * (charge_full / 1000)
+                        accum_full_mWh += (voltage_now / 1000) * (
+                            charge_full / 1000
+                        )
         except Exception as e:
             print(f"Error reading battery info: {e}", file=sys.stderr)
             return
@@ -119,22 +137,31 @@ def status_bat():
 
 
 def status_load():
+    """
+    Get load average.
+    """
     with open("/proc/loadavg", "r") as f:
         load_avg = f.read().split()[0]
     return json_output("load", f"Load: {load_avg}")
 
 
 def status_qubes():
-    running_qubes = len([vm for vm in app.domains if vm.is_running() and vm.name != "dom0"])
-    if running_qubes == 0:
-        return
-    elif running_qubes == 1:
-        return json_output("qubes", f"{running_qubes} qube")
-    else:
-        return json_output("qubes", f"{running_qubes} qubes")
+    """
+    Get amount of qubes that are running..
+    """
+    running_qubes = len(
+        [vm for vm in app.domains if vm.klass != "AdminVM" and vm.is_running()]
+    )
+    qube_string = "qube"
+    if running_qubes >= 2:
+        qube_string = "qubes"
+    return json_output("qubes", f"{running_qubes} {qube_string}")
 
 
 def status_disk():
+    """
+    Get amount of space in the default private pool.
+    """
     default_pool = app.pools[app.property_get_default("default_pool_private")]
     size = int(default_pool.size)
     usage = int(default_pool.usage)
@@ -154,6 +181,9 @@ def status_disk():
 
 
 def status_volume():
+    """
+    Get volume percentage and if muted or not.
+    """
     try:
         volcmd = subprocess.run(
             ["amixer", "sget", "Master"],
@@ -185,14 +215,19 @@ def status_volume():
 
 
 def main():
+    """
+    Query basic statistics to display on i3 status bar.
+    """
     # Send the header so that i3 bar knows we want to use JSON.
-    print('{"version": 1}', flush=True)
+    prefix = '{"version": 1}'
     # Begin the endless array.
-    print("[", flush=True)
+    prefix += "["
     # We send an empty first array of blocks to make the loop simpler.
-    print("[]", flush=True)
+    prefix += "[]"
 
-    n = 0
+    print(prefix, flush=True)
+
+    loop_count = 0
     qubesd_status = None
     qubes_status = None
     disk_status = None
@@ -201,15 +236,17 @@ def main():
     volume_status = None
     net_status = None
     while True:
-        if n % 2 == 0:
+        if loop_count % 2 == 0:
             try:
                 qubes_status = status_qubes()
                 disk_status = status_disk()
                 # network status disabled by default as it's dangerous to run a
-                # command on an untrusted qube from dom0/adminvm
+                # command on an untrusted qube from an interface qube.
                 # net_status = status_net()
             except qadmin_exc.QubesDaemonCommunicationError:
-                qubesd_status = json_output("qubesd", "qubesd connection failed")
+                qubesd_status = json_output(
+                    "qubesd", "qubesd connection failed"
+                )
             bat_status = status_bat()
             load_status = status_load()
             volume_status = status_volume()
@@ -226,11 +263,13 @@ def main():
             time_status,
             net_status,
         ]
-        final_status_list = [status for status in status_list if status is not None]
+        final_status_list = [
+            status for status in status_list if status is not None
+        ]
         print("," + json.dumps(final_status_list), flush=True)
 
         time.sleep(1)
-        n += 1
+        loop_count += 1
 
 
 if __name__ == "__main__":
